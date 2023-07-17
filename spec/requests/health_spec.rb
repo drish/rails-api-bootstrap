f0f933392f0c781da4675048ce4759e628b0004c
Derich Pacheco <carlosderich@gmail.com>
2023-07-17 18:39:04 -0300

    lint

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Healths", type: :request do
-  describe "GET /health" do
+RSpec.describe 'Healths', type: :request do
+  describe 'GET /health' do
     it 'should return 200' do
-      get("/health")
+      get('/health')
       expect(response.status).to eq 200
-      expect(JSON.parse(response.body)["ok"].to_s).to eq "true"
+      expect(JSON.parse(response.body)['ok'].to_s).to eq 'true'
     end
   end
 end
