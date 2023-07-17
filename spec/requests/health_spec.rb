88cebddeb51d54a4da6d5ebcc13bc74b447c5402
Derich Pacheco <carlosderich@gmail.com>
2023-07-17 18:05:30 -0300

    init test

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Healths", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /health" do
+    it 'should return 200' do
+      get("/health")
+      expect(response.status).to eq 200
+      expect(JSON.parse(response.body)["ok"].to_s).to eq "true"
+    end
   end
 end
